dcebde1c51182b78668f79f3c51b10df9f8415a0
bigdmike <piggy5272@gmail.com>
2022-01-28 13:08:40 +0800

    update

--- src/api/common_column.js
+++ src/api/common_column.js
@@ -15,16 +15,17 @@ export async function get_common_column(column_list) {
     });
     return res_obj
 }
-export function update_common_column(column_item) {
+export function update_common_column(column_item, front_show = "Y") {
     let data = {
         Title: column_item.Title,
         Content: column_item.Content,
-        Front: column_item.Front
+        Front: front_show
     }
     return post('admin/column/replace', data, "已成功更新欄位")
 }
-export function update_common_column_image(column_name, column_item) {
+export function update_common_column_image(column_name, column_item, front_show = "Y") {
     let image_data = {}
     typeof column_item.Image1 == "string" || column_item.Image1 == null ? "" : image_data[column_name] = column_item.Image1
+    image_data.Front = front_show
     return post_image("admin/column/image", image_data, "已成功更新欄位")
 }
